0c79d26c0f8a7d440a22a20fd80980b9b1b33b00
quietQuiet <j.lietunovas@gmail.com>
2019-08-11 21:09:55 +0200

    add working admin panel, connected to graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,5 +10,20 @@ module.exports = {
     // ... other plugins
     `gatsby-plugin-netlify-cms`,
     // ... other plugins
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/static/assets`,
+        name: "images",
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/content`,
+      },
+    },
+    `gatsby-transformer-remark`,
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,36 @@
 import React from "react"
+import { graphql } from "gatsby"
 
-export default () => <div>Hello world!</div>
+function IndexPage(props) {
+  const data = props.data.allFile.edges[0].node.childMarkdownRemark.frontmatter
+  return (
+    <>
+      <h1>{data.title}</h1>
+      <p>{data.intro}</p>
+      <div style={{ maxWidth: `300px` }}>
+        <img src={data.image} alt="weird pic" />
+      </div>
+    </>
+  )
+}
+
+export const query = graphql`
+  query {
+    allFile(
+      filter: { sourceInstanceName: { eq: "content" }, name: { eq: "home" } }
+    ) {
+      edges {
+        node {
+          childMarkdownRemark {
+            frontmatter {
+              title
+              intro
+              image
+            }
+          }
+        }
+      }
+    }
+  }
+`
+export default IndexPage
